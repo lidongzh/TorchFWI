3bdcecf5680c3153fb0ee05624f1aca43f222fa4
lidongzh <lidongzh@stanford.edu>
2021-09-24 15:58:09 -0400

    add operation of scalar tensor in PyTorch C++

--- Ops/FWI/Src/Torch_Fwi.cpp
+++ Ops/FWI/Src/Torch_Fwi.cpp
@@ -7,15 +7,15 @@
 #include <vector>
 #include "libCUFD.h"
 #include "omp.h"
-using namespace std;
+// using namespace std;
 
-std::vector<torch::Tensor> fwi_forward(torch::Tensor &th_Lambda,
-                                       torch::Tensor &th_Mu,
-                                       torch::Tensor &th_Den,
-                                       torch::Tensor &th_stf, int gpu_id,
-                                       torch::Tensor &th_shot_ids,
+std::vector<torch::Tensor> fwi_forward(const torch::Tensor &th_Lambda,
+                                       const torch::Tensor &th_Mu,
+                                       const torch::Tensor &th_Den,
+                                       const torch::Tensor &th_stf, int gpu_id,
+                                       const torch::Tensor &th_shot_ids,
                                        const string para_fname) {
-  float *misfit_ptr = NULL;
+  float *misfit_ptr = nullptr;
   misfit_ptr = (float *)malloc(sizeof(float));
   // transform from torch tensor to 1D native array
   auto Lambda = th_Lambda.data_ptr<float>();
@@ -24,17 +24,17 @@ std::vector<torch::Tensor> fwi_forward(torch::Tensor &th_Lambda,
   auto stf = th_stf.data_ptr<float>();
   auto shot_ids = th_shot_ids.data_ptr<int>();
   const int group_size = th_shot_ids.size(0);
-  cufd(misfit_ptr, NULL, NULL, NULL, NULL, Lambda, Mu, Den, stf, 0, gpu_id,
+  cufd(misfit_ptr, nullptr, nullptr, nullptr, nullptr, Lambda, Mu, Den, stf, 0, gpu_id,
        group_size, shot_ids, para_fname);
   torch::Tensor th_misfit = torch::from_blob(misfit_ptr, {1});
   return {th_misfit.clone()};
 }
 
-std::vector<torch::Tensor> fwi_backward(torch::Tensor th_Lambda,
-                                        torch::Tensor th_Mu,
-                                        torch::Tensor th_Den,
-                                        torch::Tensor th_stf, int ngpu,
-                                        torch::Tensor th_shot_ids,
+std::vector<torch::Tensor> fwi_backward(const torch::Tensor &th_Lambda,
+                                        const torch::Tensor &th_Mu,
+                                        const torch::Tensor &th_Den,
+                                        const torch::Tensor &th_stf, int ngpu,
+                                        const torch::Tensor &th_shot_ids,
                                         const string para_fname) {
   const int nz = th_Lambda.size(0);
   const int nx = th_Lambda.size(1);
@@ -52,7 +52,7 @@ std::vector<torch::Tensor> fwi_backward(torch::Tensor th_Lambda,
   auto stf = th_stf.data_ptr<float>();
   auto sepBars = torch::linspace(0, group_size, ngpu + 1,
                                  torch::TensorOptions().dtype(torch::kFloat32));
-  std::vector<torch::Tensor> vec_misfit(ngpu);
+  std::vector<float> vec_misfit(ngpu);
   std::vector<torch::Tensor> vec_grad_Lambda(ngpu);
   std::vector<torch::Tensor> vec_grad_Mu(ngpu);
   std::vector<torch::Tensor> vec_grad_Den(ngpu);
@@ -83,23 +83,23 @@ std::vector<torch::Tensor> fwi_backward(torch::Tensor th_Lambda,
     vec_grad_Mu.at(i) = th_grad_Mu;
     vec_grad_Den.at(i) = th_grad_Den;
     vec_grad_stf.at(i) = th_grad_stf;
-    torch::Tensor th_misfit = torch::from_blob(&misfit, {1});
-    vec_misfit.at(i) = th_misfit;
+    // torch::Tensor th_misfit = torch::from_blob(&misfit, {1});
+    vec_misfit.at(i) = misfit;
   }
-
+  float misfit_sum = 0.0;
   for (int i = 0; i < ngpu; i++) {
     th_grad_Lambda_sum += vec_grad_Lambda.at(i);
     th_grad_Mu_sum += vec_grad_Mu.at(i);
     th_grad_Den_sum += vec_grad_Den.at(i);
-    th_misfit_sum += vec_misfit.at(i);
+    misfit_sum += vec_misfit.at(i);
   }
-  return {th_misfit_sum.clone(), th_grad_Lambda_sum, th_grad_Mu_sum,
+  return {torch::from_blob(&misfit_sum, {1}), th_grad_Lambda_sum, th_grad_Mu_sum,
           th_grad_Den_sum, vec_grad_stf.at(0)};
 }
 
-void fwi_obscalc(torch::Tensor th_Lambda, torch::Tensor th_Mu,
-                 torch::Tensor th_Den, torch::Tensor th_stf, int ngpu,
-                 torch::Tensor th_shot_ids, const string para_fname) {
+void fwi_obscalc(const torch::Tensor &th_Lambda, const torch::Tensor &th_Mu,
+                 const torch::Tensor &th_Den, const torch::Tensor &th_stf, int ngpu,
+                 const torch::Tensor &th_shot_ids, const string para_fname) {
   const int group_size = th_shot_ids.size(0);
   if (ngpu > group_size) {
     printf("The number of GPUs should be smaller than the number of shots!\n");
@@ -118,7 +118,7 @@ void fwi_obscalc(torch::Tensor th_Lambda, torch::Tensor th_Mu,
     int endBar = round(sepBars[i + 1].item<int>());
     auto th_sub_shot_ids = th_shot_ids.narrow(0, startBar, endBar - startBar);
     auto shot_ids = th_sub_shot_ids.data_ptr<int>();
-    cufd(NULL, NULL, NULL, NULL, NULL, Lambda, Mu, Den, stf, 2, i,
+    cufd(nullptr, nullptr, nullptr, nullptr, nullptr, Lambda, Mu, Den, stf, 2, i,
          th_sub_shot_ids.size(0), shot_ids, para_fname);
     //     cufd(NULL, NULL, NULL, NULL, NULL, Lambda, Mu, Den, stf, 2, gpu_id,
     //          group_size, shot_ids, para_fname);
